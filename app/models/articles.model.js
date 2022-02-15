eb35496bca46c010348cb545b2a2e249b4424a17
ActuallySilver <jason@educateit.net>
2022-02-15 15:23:43 +0000

    Added a ; to a select statement

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -1,7 +1,7 @@
 const db = require("../../db/connection");
 exports.selectArticleById = async (id) => {
   const { rows: articles } = await db.query(
-    "SELECT * FROM articles WHERE article_id = $1",
+    "SELECT * FROM articles WHERE article_id = $1;",
     [id]
   );
   const [article] = articles;
