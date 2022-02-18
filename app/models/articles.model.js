740ac2de5fb30068d7b3599f499b62cc318bcf41
ActuallySilver <jason@educateit.net>
2022-02-18 14:37:16 +0000

    complete ticket #16 - allow sort_by, order and topic queries

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -1,8 +1,7 @@
 const db = require("../../db/connection");
 exports.selectArticleById = async (id) => {
-
   const { rows: articles } = await db.query(
-    `SELECT articles.article_id, articles.author, articles.body, articles.created_at, articles.title, articles.topic, articles.votes, COUNT(comments.article_id) AS comment_count
+    `SELECT articles.article_id, articles.author, articles.body, articles.created_at, articles.title, articles.topic, articles.votes, COUNT(comments.article_id)::int AS comment_count
      FROM articles
      LEFT JOIN comments ON comments.article_id = articles.article_id
      WHERE articles.article_id = $1
@@ -22,13 +21,23 @@ exports.updateArticleVotes = async (id, votes) => {
   return article;
 };
 
-exports.selectArticles = async () => {
-  const { rows: articles } = await db.query(
-    `SELECT articles.article_id, articles.author, articles.created_at, articles.title, articles.topic, articles.votes, COUNT(comments.article_id)::int AS comment_count
-     FROM articles
-     LEFT JOIN comments ON comments.article_id = articles.article_id
-     GROUP BY articles.article_id
-     ORDER BY created_at DESC;`
-  );
+exports.selectArticles = async (sort_by = "created_at", order = "DESC", topic) => {
+  const availableQueries = {
+    sort_by: ["title", "topic", "author", "body", "created_at", "votes"],
+    order: ["ASC", "DESC"],
+    topic: ["mitch", "cats"],
+  };
+  let queryStr = `SELECT articles.article_id, articles.author, articles.created_at, articles.title, articles.topic, articles.votes, COUNT(comments.article_id)::int AS comment_count
+  FROM articles
+  LEFT JOIN comments ON comments.article_id = articles.article_id`;
+  if (availableQueries.topic.indexOf(topic) !== -1) queryStr += ` WHERE articles.topic = '${topic}'`;
+  else if (topic) return Promise.reject({ status: 404, errMsg: "topic not found" });
+  queryStr += " GROUP BY articles.article_id";
+  if (availableQueries.sort_by.indexOf(sort_by) !== -1) queryStr += ` ORDER BY articles.${sort_by}`;
+  else return Promise.reject({ status: 400, errMsg: "invalid sort_by" });
+  if (availableQueries.order.indexOf(order) !== -1) queryStr += ` ${order}`;
+  else return Promise.reject({ status: 400, errMsg: "invalid order" });
+  queryStr += ";";
+  const { rows: articles } = await db.query(queryStr);
   return articles;
 };
