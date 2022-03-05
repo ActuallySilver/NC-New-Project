f34967832b1956a254f70f6f42bf9b67ce37fe9a
ActuallySilver <jason@educateit.net>
2022-03-05 20:37:35 +0000

    Added startup settings for heroku

--- app/app.js
+++ app/app.js
@@ -6,4 +6,8 @@ app.use("/api", apiRouter);
 //path not found
 app.all("*", controllers.errors.handle404);
 
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
 module.exports = app;
