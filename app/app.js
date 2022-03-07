b5a038242ffc9d5f5d52bf1cf8d6215015b32ab3
ActuallySilver <jason@educateit.net>
2022-03-07 21:00:38 +0000

    Added cors middleware

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const controllers = require("./controllers");
 const app = express();
 const apiRouter = require("./routes/api.router");
+const cors = require("cors");
+
+app.use(cors());
+
 app.use("/api", apiRouter);
 //path not found
 app.all("*", controllers.errors.handle404);
